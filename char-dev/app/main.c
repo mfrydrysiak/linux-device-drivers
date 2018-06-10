ce804b374bd40c5d6d3ed99599150f2892cfd409
Marek Frydrysiak <marek.frydrysiak@gmail.com>
2018-06-10 13:24:33 +0200

    Send data from user-space app to kernel module
    Testing app sends "Hello" message to the kernel device driver.

--- char-dev/app/main.c
+++ char-dev/app/main.c
@@ -1,18 +1,29 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include <stdio.h>
+
+#define ARRAY_COUNT(x) (sizeof(x)/sizeof(x[0]))
 
 int main()
 {
     const char * driver_path = "/dev/mfchar";
-    FILE * fd = NULL;
+    const char send_buffer[] = {"Hello Kernel!"};
+    ssize_t count;
+    int fd;
 
-    fd = fopen(driver_path, "rw");
-    if (fd == NULL) {
+    fd = open(driver_path, O_RDWR);
+    if (fd < 0) {
         printf("Could not open the device!\n");
         return 1;
     }
     printf("Device successfully opened.\n");
 
-    fclose(fd);
+    count = write(fd, send_buffer, ARRAY_COUNT(send_buffer));
+    printf("Written %ld characters to the device.\n", count);
+
+    close(fd);
     printf("Device closed.\n");
 
     return 0;
